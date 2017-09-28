7b277948c52563b1e6c02ffa94d5dc13f26c462a
Hunter Maddux <phmaddux@gmail.com>
2017-09-28 16:27:07 -0400

    Worked out first pass of the needed Schema

---
+++ db/schema.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+// Schema constructors, defined by mongoose
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    genre:{
+        type: String,
+        required: false
+    },
+    gameType: {
+        type: String,
+        required: false
+    },
+    players: {
+        type: Number,
+        required: false
+    },
+    picture: {
+        type: String,
+        reuired: false
+    },
+});
+
+const PlayedSchema = new Schema({
+    timesPlayed: {
+        type: Number,
+        required: false
+    },
+    averagePlayTime:{
+        type: Number,
+        required: false
+    },
+    Thoughts: {
+        type: String,
+        required: false
+    },
+    games: [GameSchema]
+});
+
+const WishlistSchema = new Schema({
+    theHype: {
+        type: String,
+        required: false
+    },
+    games: [GameSchema]    
+});
+
+// Creating the models for each schema
+const GameModel = mongoose.model('Game', GameSchema)
+const PlayedModel = mongoose.model('Played', PlayedSchema)
+const WishlistModel = mongoose.model('Wishlist', WishlistSchema)
+
+// export the models so they are able to be required in other parts
+module.exports = {
+    GameModel: GameModel,
+    PlayedModel: PlayedModel,
+    WishlistModel: WishlistModel
+}
