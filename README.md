712be7e140f55b346802953fc99eaae947be15fd
Hunter Maddux <phmaddux@gmail.com>
2017-09-28 15:59:11 -0400

    worked on the Readme, filling out app.js, requiring

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Project_Two_Lets_Play
-Project Two for GA my first pass at my Let's Play idea.
+Project Two for GA. This is the first pass at my 'Let's Play' idea.
+This is going to be a site which allows the user to catalouge any board games that the user has played recently.
--- app.js
+++ app.js
@@ -6,17 +6,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 // Database Set Up
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI); 
 
-
+const db =mongoose.connection
+db.on('error', (error => {
+  console.log(error)
+}))
+db.once('open', () => {
+  console.log('Connected to MongoDB!')
+})
 
 var app = express();
+
+app.use(methodOverride('_method'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,8 +37,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Resgister Controllers
+// unsure of the app.use section
+var indexController = require('./routes/indexController');
+app.use('/', indexController);
+
+const playedController = require('./routes/playedController')
+app.use('/', playedController)
+
+const gameController = require('./routes/gameController')
+app.use('/', gameController)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,10 +60,17 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 module.exports = app;
+
+// code that needs to go?
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+
+// app.use('/', index);
+// app.use('/users', users);
