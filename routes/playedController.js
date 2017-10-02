59e04bdeb56ab73c42e9036227edb6551e99ee05
Hunter Maddux <phmaddux@gmail.com>
2017-10-02 15:44:28 -0400

    Edit Route fixed

--- routes/playedController.js
+++ routes/playedController.js
@@ -17,7 +17,7 @@ router.get('/', (request, response) => {
             // THEN once they come back from the database
             // RENDER them in Handlebars
             response.render('played/index', {
-                played: played
+                playedGame: played[0]
             })
         })
             .catch((error) => {
@@ -51,7 +51,7 @@ router.post('/', (request, response) => {
 })
 
 // Edit route
-router.get('/:gamesId/edit', (request, respone) => {
+router.get('/:playedId/games/:gamesId/edit', (request, response) => {
     // get played Id
     const playedId = request.params.playedId
     // get game Id
@@ -65,7 +65,7 @@ router.get('/:gamesId/edit', (request, respone) => {
 
             // render prefilled form
             // and pass played Id
-            response.render('games/edit', {
+            response.render('played/edit', {
             game: game,
             playedId: playedId,
         })
@@ -75,6 +75,60 @@ router.get('/:gamesId/edit', (request, respone) => {
     })
 })
 
+// Update route
+router.put('/:gamesId', (request, response) => {
+    // get played Id from parameters
+    const playedId = request.params.playedId
+    // get game Id fromt the parameters
+    const gamesId = request.params.gamesId
+    // get updated game from request body
+    const updatedGame = request.body
+
+    PlayedModel.findById(playedId)
+        .then((played) => {
+            // after played has been returned
+            // find the game by id from the played list
+            const game = played.games.id(gameId)
+
+            game.name = updatedGame.name,
+            game.picture = updatedPicture.picture,
+            game.genre = updatedGenre.genre,
+            game.gameType = updatedGameType.gameType,
+            game.players = updatedPlayers.players,
+            game.timesPlayed = updatedTimesPlayed.timesPlayed,
+            game.averagePlayTime = updatedAveragePlayTime.averagePlayTime,
+            game.thoughts = updatedthoughts.thoughts,
+            game.theHype = updatedTheHype.theHype
+            
+            return played.save()
+        })
+        .then(() => {
+        // save and redirect
+        response.redirect(`/played/${playedId}/games/${gamesId}`)
+        })
+
+})
+
+// Show route
+router.get('/:gamesId', (request, response) => {
+    // Get company ID from the paramaters
+    const playedId = request.params.playedId
+    // Get the Game Id from the paramaters
+    const gamesId = request.params.gamesId
+    PlayedModel.findById(playedId)
+        .then((played) => {
+
+            const game = played.games.id(gamesId)
+
+            response.render('games/show', {
+                game: game,
+                played: played
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
 
 
 module.exports = router
