e1382fa117e11a60e8671bd53d54cc7303c1e917
Hunter Maddux <phmaddux@gmail.com>
2017-10-02 13:29:52 -0400

    finished new route for played

--- routes/playedController.js
+++ routes/playedController.js
@@ -25,4 +25,56 @@ router.get('/', (request, response) => {
     })
 })
 
-module.exports = router;
+// New game
+router.get('/new', (request, response) => {
+    response.render('played/new')
+})
+
+// create route
+router.post('/', (request, response) => {
+    const playedId  = request.params.playedId
+    // get new game info
+    const newGame = request.body
+    // create new game
+    PlayedModel.create(newGame)
+        .then((played) => {
+            played.games.push(newGame)
+            return played.save()
+        })
+        .then((played) => {
+            // after saving model, redirect to index
+            response.redirect('/played')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+// Edit route
+router.get('/:gamesId/edit', (request, respone) => {
+    // get played Id
+    const playedId = request.params.playedId
+    // get game Id
+    const gamesId = request.params.gamesId
+    // use played Id to find the played list
+    PlayedModel.findById(playedId)
+        .then((played) => {
+            // after played is returned
+            // then find game by id
+            const game = played.games.id(gamesId)
+
+            // render prefilled form
+            // and pass played Id
+            response.render('games/edit', {
+            game: game,
+            playedId: playedId,
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+
+
+module.exports = router
